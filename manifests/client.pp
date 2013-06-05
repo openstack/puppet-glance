1028e92f81f92b36f678a1b0acb3cd06788cd97a
Mathieu Gagné <mgagne@iweb.com>
2013-06-05 13:19:54 -0400

    Add missing glance::params include in client
    Change-Id: Ia5f99939f10a78033b743aca4a289b1e8d314e02

--- manifests/client.pp
+++ manifests/client.pp
@@ -8,6 +8,8 @@ class glance::client (
   $ensure = 'present'
 ) {
 
+  include glance::params
+
   package { 'python-glance':
     name   => $::glance::params::client_package_name,
     ensure => $ensure,
