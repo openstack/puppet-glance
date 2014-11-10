e90ea396267f960ec3379e36e42600078548de67
Yanis Guenane <yanis.guenane@enovance.com>
2014-11-10 14:19:13 -0500

    policy.pp : Fix duplicate ressource issue openstacklib::policy
    Currently, in the policy.pp file the class openstacklib::policy
    is called. The same apply for all components resulting in a
    Duplicate Ressource issue. Using directly the underlying ressource
    openstacklib::policy::base we prevent this issue.
    
    Change-Id: I08a9aa3d296042292b0127b9920dd7971eec57fe

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -18,11 +18,12 @@ class glance::policy (
   $policy_path = '/etc/glance/policy.json',
 ) {
 
+  validate_hash($policies)
+
   Openstacklib::Policy::Base {
     file_path => $policy_path,
   }
-  class { 'openstacklib::policy' :
-    policies => $policies,
-  }
+
+  create_resources('openstacklib::policy::base', $policies)
 
 }
