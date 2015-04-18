62c42a5f6122d146f8c4e292c878cdd3e958a85f
Colleen Murphy <colleen@puppetlabs.com>
2015-04-17 17:42:55 -0700

    Revert "Revert "Fix os_region_name in provider""
    Depends on https://review.openstack.org/#/c/174969/
    
    This reverts commit e2301958e9dd4086fb6f5689499e5bb39e87e208.
    
    Change-Id: I86db306b90cfdba53cf40bd9e3b2cce453e821d0

--- spec/unit/provider/glance_spec.rb
+++ spec/unit/provider/glance_spec.rb
@@ -36,7 +36,7 @@ describe Puppet::Provider::Glance do
                'admin_user'        => 'user',
                'admin_password'    => 'pass'
             },
-                'DEFAULT' =>
+                'glance_store' =>
             {
                 'os_region_name' => 'SomeRegion',
             }
