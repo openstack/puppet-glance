93f79bb56d7e761c1728beb050b661760683dbd6
Colleen Murphy <colleen@gazlene.net>
2015-05-13 17:32:19 -0700

    Add acceptance test for glance image
    Change-Id: Ie74323930f100c71197c8bd10bae5a3975fabe50

--- spec/acceptance/basic_glance_spec.rb
+++ spec/acceptance/basic_glance_spec.rb
@@ -81,6 +81,14 @@ describe 'glance class' do
         verbose             => false,
         keystone_password   => 'a_big_secret',
       }
+
+      glance_image { 'test_image':
+        ensure           => present,
+        container_format => 'bare',
+        disk_format      => 'qcow2',
+        is_public        => 'yes',
+        source           => 'http://download.cirros-cloud.net/0.3.1/cirros-0.3.1-x86_64-disk.img',
+      }
       EOS
 
       # Run it twice and test for idempotency
@@ -88,5 +96,13 @@ describe 'glance class' do
       apply_manifest(pp, :catch_changes => true)
     end
 
+    describe 'glance images' do
+      it 'should create a glance image' do
+        shell('openstack --os-username glance --os-password a_big_secret --os-tenant-name services --os-auth-url http://127.0.0.1:5000/v2.0 image list') do |r|
+          expect(r.stdout).to match(/test_image/)
+          expect(r.stderr).to be_empty
+        end
+      end
+    end
   end
 end
