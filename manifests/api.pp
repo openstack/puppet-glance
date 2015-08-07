b9d8442ca246a9937a1bbe2663d691071254bfd4
Sergey Kolekonov <skolekonov@mirantis.com>
2015-08-07 15:44:19 +0300

    Add an ability to manage use_stderr parameter
    When logging is already set up, it can be useful to disable this option
    to avoid logs duplication (collected by Upstart, for example)
    
    Change-Id: I1787191d9a512fdd3c3f79d6fadda110c5e41d54
    Closes-bug: #1482564
    (cherry picked from commit 1f4a545247049eebff60d7946fe3f648fce583b6)

--- manifests/api.pp
+++ manifests/api.pp
@@ -120,6 +120,10 @@
 #   (optional) Use syslog for logging.
 #   Defaults to false.
 #
+# [*use_stderr*]
+#   (optional) Use stderr for logging
+#   Defaults to true
+#
 # [*log_facility*]
 #   (optional) Syslog facility to receive log lines.
 #   Defaults to 'LOG_USER'.
@@ -185,6 +189,7 @@ class glance::api(
   $keystone_user            = 'glance',
   $enabled                  = true,
   $use_syslog               = false,
+  $use_stderr               = true,
   $log_facility             = 'LOG_USER',
   $show_image_direct_url    = false,
   $purge_config             = false,
@@ -267,6 +272,7 @@ class glance::api(
   glance_api_config {
     'DEFAULT/verbose':               value => $verbose;
     'DEFAULT/debug':                 value => $debug;
+    'DEFAULT/use_stderr':            value => $use_stderr;
     'DEFAULT/bind_host':             value => $bind_host;
     'DEFAULT/bind_port':             value => $bind_port;
     'DEFAULT/backlog':               value => $backlog;
