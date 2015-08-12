7128d96ddea449596f345dd7222b055417e70f3a
Benedikt von St. Vieth <b.von.st.vieth@fz-juelich.de>
2015-08-12 08:21:31 +0200

    solve duplicate declaration issue for python-openstackclient
    when having a controller node with several modules that use puppet-openstacklib
    the package python-openstackclient is already declared. because openstacklib
    adds the tag 'openstack' to the resource, we have to do it here to. otherwise
    ensure_resource does not recognize it and we have a duplicate declaration
    for the package.
    
    Change-Id: Icf341ea92764f2ddb60b0e7ace09a56febe59545
    Closes-Bug: #1483663

--- spec/classes/glance_spec.rb
+++ spec/classes/glance_spec.rb
@@ -31,7 +31,9 @@ describe 'glance' do
         'mode'    => '0770'
       )}
 
-      it { is_expected.to contain_package('python-openstackclient') }
+      it { is_expected.to contain_package('python-openstackclient').with(
+        :tag => 'openstack'
+      )}
 
     end
   end
