dd53e39b11ed87e48593c28a80c4590f924c3e28
Denis Egorenko <degorenko@mirantis.com>
2015-12-03 19:35:57 +0300

    Remove qpid messaging support
    Qpid messaging driver is removed from Oslo.messaging [1]
    Therefore, we cannot support it anymore.
    
    [1] I4a9cba314c4a2f24307504fa7b5427424268b114
    
    Change-Id: I5a702aacf77956a6c616fd98d2f942654548f00d

--- manifests/notify/qpid.pp
+++ manifests/notify/qpid.pp
@@ -1,6 +1,7 @@
 # == Class: glance::notify::qpid
 #
 # used to configure qpid notifications for glance
+# Deprecated class
 #
 # === Parameters:
 #
@@ -24,20 +25,12 @@
 #   Defaults to tcp.
 #
 class glance::notify::qpid(
-  $qpid_password,
-  $qpid_username = 'guest',
-  $qpid_hostname = 'localhost',
-  $qpid_port     = '5672',
-  $qpid_protocol = 'tcp'
+  $qpid_password = undef,
+  $qpid_username = undef,
+  $qpid_hostname = undef,
+  $qpid_port     = undef,
+  $qpid_protocol = undef
 ) inherits glance::api {
 
-  glance_api_config {
-    'DEFAULT/notifier_driver':           value => 'qpid';
-    'oslo_messaging_qpid/qpid_hostname': value => $qpid_hostname;
-    'oslo_messaging_qpid/qpid_port':     value => $qpid_port;
-    'oslo_messaging_qpid/qpid_protocol': value => $qpid_protocol;
-    'oslo_messaging_qpid/qpid_username': value => $qpid_username;
-    'oslo_messaging_qpid/qpid_password': value => $qpid_password, secret => true;
-  }
-
+  warning('Qpid driver is removed from Oslo.messaging in the Mitaka release')
 }
