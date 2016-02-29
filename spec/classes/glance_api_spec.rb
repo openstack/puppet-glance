ee9e375116ebfc7739429477b14eddd989457c6c
Emilien Macchi <emilien@redhat.com>
2016-02-29 12:55:22 -0500

    Supports IPv6 in registry_host
    This patch add support for IPv6 address in registry_host.
    It adds brackets if an IPv6 is detected.
    
    Change-Id: Ic1d75984fac7bce3d0239df54a877ae2b713c442
    (cherry picked from commit 624f522e7156bcdd103a19177be4881e12ff9d49)

--- spec/classes/glance_api_spec.rb
+++ spec/classes/glance_api_spec.rb
@@ -374,6 +374,16 @@ describe 'glance::api' do
     end
   end
 
+  describe 'Support IPv6' do
+    let :params do
+      default_params.merge({
+        :registry_host => '2001::1',
+      })
+    end
+    it { is_expected.to contain_glance_api_config('DEFAULT/registry_host').with(
+      :value => '[2001::1]'
+    )}
+  end
 
   describe 'on Debian platforms' do
     let :facts do
