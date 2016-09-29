0fc2099d127b3a5d459f5591f4380d110cc92e4c
Sharat Sharma <sharat.sharma@nectechnologies.in>
2016-09-29 12:11:05 +0530

    Added retries for db_sync
    If db sync fails, it is never retried leading to errors. So, this
    patch adds retries for db_sync to avoid sync fails.
    
    Change-Id: I8778bd1b609bc5458a250111dc0a0f9b2fb52161
    Closes-Bug: #1628580

--- manifests/db/sync.pp
+++ manifests/db/sync.pp
@@ -21,6 +21,8 @@ class glance::db::sync(
     path        => '/usr/bin',
     user        => 'glance',
     refreshonly => true,
+    try_sleep   => 5,
+    tries       => 10,
     logoutput   => on_failure,
     subscribe   => [
       Anchor['glance::install::end'],
