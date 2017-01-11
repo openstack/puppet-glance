dac86c32cd32fe04cbe3b2ef35fd7bef55c406b7
Matthew J. Black <mjblack@gmail.com>
2017-01-11 11:42:45 -0500

    Allow python-memcache install from authtoken class
    The python-memcache package is required if using memcached. By
    default the package is not installed and the define has it set to
    false. This change allows managing the python-memcache package
    install from the authtoken class.
    
    Change-Id: I7de3338061bad949f26ed0d84782124c7b61eb70
    (cherry picked from commit a964f9f925c2cf28536370502a7414b1bec98975)

--- spec/classes/glance_api_authtoken_spec.rb
+++ spec/classes/glance_api_authtoken_spec.rb
@@ -83,6 +83,7 @@ describe 'glance::api::authtoken' do
           :memcache_security_strategy           => 'ENCRYPT',
           :memcache_use_advanced_pool           => true,
           :memcached_servers                    => ['memcached01:11211','memcached02:11211'],
+          :manage_memcache_package              => true,
           :region_name                          => 'region2',
           :revocation_cache_time                => '11',
           :signing_dir                          => '/var/cache',
@@ -127,6 +128,11 @@ describe 'glance::api::authtoken' do
         is_expected.to contain_glance_api_config('keystone_authtoken/signing_dir').with_value(params[:signing_dir])
         is_expected.to contain_glance_api_config('keystone_authtoken/token_cache_time').with_value(params[:token_cache_time])
       end
+
+      it 'installs python memcache package' do
+        is_expected.to contain_package('python-memcache')
+      end
+
     end
   end
 
