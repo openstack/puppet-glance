384891ba49cd548d5e6325c45dd05b9441ed1851
Harry Rybacki <hrybacki@redhat.com>
2017-10-06 09:50:29 -0400

    Configure *_domain_name to Default by default
    Keystone v2.0 API was removed so we have no choice but configuring
    user_domain_name and project_domain_name otherwise it fallbacks to
    Keystone v2.0 and it fails. This patch sets the default value so we make
    sure Keystone v3 will be used out of the box for our users.
    
    Change-Id: If0a614520c4737e489147e18b1e9028e1f671f88

--- manifests/api/authtoken.pp
+++ manifests/api/authtoken.pp
@@ -22,11 +22,11 @@
 #
 # [*user_domain_name*]
 #   (Optional) Name of domain for $username
-#   Defaults to $::os_service_default
+#   Defaults to 'Default'
 #
 # [*project_domain_name*]
 #   (Optional) Name of domain for $project_name
-#   Defaults to $::os_service_default
+#   Defaults to 'Default'
 #
 # [*insecure*]
 #   (Optional) If true, explicitly allow TLS without checking server cert
@@ -192,8 +192,8 @@ class glance::api::authtoken(
   $password                       = $::os_service_default,
   $auth_url                       = 'http://127.0.0.1:35357',
   $project_name                   = 'services',
-  $user_domain_name               = $::os_service_default,
-  $project_domain_name            = $::os_service_default,
+  $user_domain_name               = 'Default',
+  $project_domain_name            = 'Default',
   $insecure                       = $::os_service_default,
   $auth_section                   = $::os_service_default,
   $auth_type                      = 'password',
