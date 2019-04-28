677a307b06e6ba0b65e2874cb1fd916d7f3f4daf
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:08 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: I02536803fad90dbf6edf27a85786f6e94d53d2f1
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- spec/classes/glance_registry_db_spec.rb
+++ spec/classes/glance_registry_db_spec.rb
@@ -6,45 +6,45 @@ describe 'glance::registry::db' do
       it { should contain_class('glance::deps') }
 
       it { should contain_oslo__db('glance_registry_config').with(
-        :db_max_retries => '<SERVICE DEFAULT>',
-        :connection     => 'sqlite:///var/lib/glance/glance.sqlite',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :db_max_retries          => '<SERVICE DEFAULT>',
+        :connection              => 'sqlite:///var/lib/glance/glance.sqlite',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
         {
-          :database_db_max_retries => '-1',
-          :database_connection     => 'mysql+pymysql://glance_registry:glance@localhost/glance',
-          :database_idle_timeout   => '3601',
-          :database_min_pool_size  => '2',
-          :database_max_retries    => '11',
-          :database_retry_interval => '11',
-          :database_max_pool_size  => '11',
-          :database_max_overflow   => '21',
-          :database_pool_timeout   => '21',
+          :database_db_max_retries          => '-1',
+          :database_connection              => 'mysql+pymysql://glance_registry:glance@localhost/glance',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_max_retries             => '11',
+          :database_retry_interval          => '11',
+          :database_max_pool_size           => '11',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
         }
       end
 
       it { should contain_class('glance::deps') }
 
       it { should contain_oslo__db('glance_registry_config').with(
-        :db_max_retries => '-1',
-        :connection     => 'mysql+pymysql://glance_registry:glance@localhost/glance',
-        :idle_timeout   => '3601',
-        :min_pool_size  => '2',
-        :max_pool_size  => '11',
-        :max_retries    => '11',
-        :retry_interval => '11',
-        :max_overflow   => '21',
-        :pool_timeout   => '21',
+        :db_max_retries          => '-1',
+        :connection              => 'mysql+pymysql://glance_registry:glance@localhost/glance',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '11',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end
