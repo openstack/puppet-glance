43f3d5eab2b282d5baa6449f775ed64ee8eb59d6
Takashi Kajinami <tkajinam@redhat.com>
2020-04-14 17:20:19 +0900

    Fix outdated get_glance_crednetials in Puppet::Provider::Glance
    This patch is a cherry pick of the change in master[2] to update
    outdated provider implementation, and introduce the implementetion
    consisntent with other modules like puppet-nova or puppet-neutron.
    
    [1] 066f45758d974bb6343c0b92583c8185ef588823
    
    Closes-Bug: #1872691
    Change-Id: I1dfcb311c10c633788c24484c21277255b60e4e5

--- spec/unit/provider/glance_spec.rb
+++ spec/unit/provider/glance_spec.rb
@@ -3,33 +3,64 @@ require 'spec_helper'
 require 'puppet/provider/glance'
 require 'tempfile'
 
-
-klass = Puppet::Provider::Glance
-
-class Puppet::Provider::Glance
-  def self.reset
-    @admin_endpoint = nil
-    @tenant_hash    = nil
-    @admin_token    = nil
-    @keystone_file  = nil
-  end
-end
-
 describe Puppet::Provider::Glance do
+
+  def klass
+    described_class
+  end
+
+  let :credential_hash do
+    {
+      'auth_url'     => 'https://192.168.56.210:5000/v3/',
+      'project_name' => 'admin_tenant',
+      'username'     => 'admin',
+      'password'     => 'password',
+      'region_name'  => 'Region1',
+    }
+  end
+
+  let :auth_endpoint do
+    'https://192.168.56.210:5000/v3/'
+  end
+
+  let :credential_error do
+    /Glance types will not work/
+  end
 
   after :each do
     klass.reset
   end
 
-  describe 'when retrieving the auth credentials' do
+  describe 'when determining credentials' do
 
-    it 'should fail if no auth params are passed and the glance config file does not have the expected contents' do
-      mock = {}
-      Puppet::Util::IniConfig::File.expects(:new).returns(mock)
-      mock.expects(:read).with('/etc/glance/glance-api.conf')
+    it 'should fail if config is empty' do
+      conf = {}
+      klass.expects(:glance_conf).returns(conf)
       expect do
         klass.glance_credentials
-      end.to raise_error(Puppet::Error, /does not contain all required sections/)
+      end.to raise_error(Puppet::Error, credential_error)
+    end
+
+    it 'should fail if config does not have keystone_authtoken section.' do
+      conf = {'foo' => 'bar'}
+      klass.expects(:glance_conf).returns(conf)
+      expect do
+        klass.glance_credentials
+      end.to raise_error(Puppet::Error, credential_error)
+    end
+
+    it 'should fail if config does not contain all auth params' do
+      conf = {'keystone_authtoken' => {'invalid_value' => 'foo'}}
+      klass.expects(:glance_conf).returns(conf)
+      expect do
+       klass.glance_credentials
+      end.to raise_error(Puppet::Error, credential_error)
+    end
+
+    it 'should use specified uri in the auth endpoint' do
+      conf = {'keystone_authtoken' => credential_hash}
+      klass.expects(:glance_conf).returns(conf)
+      expect(klass.get_auth_endpoint).to eq(auth_endpoint)
     end
 
   end
