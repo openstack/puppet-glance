043fa449a982dc0c6ca9173a745d29bc869279da
Takashi Kajinami <tkajinam@redhat.com>
2020-07-08 11:02:47 +0900

    Add support for the interface parameter in authtoken middleware
    This patch adds support for [keystone_authtoken] interface parameter,
    so that operators can define which endpoint should be used by authtoken
    middleware.
    
    Change-Id: I380868884abe92b35e93c3bf22d877838d0eac55
    (cherry picked from commit 0b973b4648dd0f5cc74ae70588a1e37f7636a0af)
    (cherry picked from commit a15022400a0001e5041381af266adc3ba810d332)

--- manifests/registry/authtoken.pp
+++ manifests/registry/authtoken.pp
@@ -177,6 +177,11 @@
 #   true/false
 #   Defaults to $::os_service_default.
 #
+# [*interface*]
+#  (Optional) Interface to use for the Identity API endpoint. Valid values are
+#  "public", "internal" or "admin".
+#  Defaults to $::os_service_default.
+#
 class glance::registry::authtoken(
   $username                       = 'glance',
   $password                       = $::os_service_default,
@@ -212,6 +217,7 @@ class glance::registry::authtoken(
   $token_cache_time               = $::os_service_default,
   $service_token_roles            = $::os_service_default,
   $service_token_roles_required   = $::os_service_default,
+  $interface                      = $::os_service_default,
 ) {
 
   include ::glance::deps
@@ -255,5 +261,6 @@ class glance::registry::authtoken(
       token_cache_time               => $token_cache_time,
       service_token_roles            => $service_token_roles,
       service_token_roles_required   => $service_token_roles_required,
+      interface                      => $interface,
     }
 }
