openstack/puppet-glance

commit 2936eb233e0310c0788a3c8ed05c42af388dc6e2

Add config parameter for cinder mounting needs
This patch is introducing cinder_mount_point_base parameter into
glance::backend::multistore::cinder class, so that operators can
provide the cinder mount point for NFS volumes.

Change-Id: Ib724f6e895716847827be48ed08b01a7a312338f
(cherry picked from commit 46e9319d8e012d2df6b7d8f85f59315db6e95ff2)
(cherry picked from commit 11bd956b9b6ae6e09547bfe14ac02af4ade573ce)
(cherry picked from commit 1bd1615234f261e2df8cda04fd6581230b758e6b)

manifests/backend/multistore/cinder.pp
@@ -66,6 +66,11 @@
 #   (optional) A valid password for the user specified by `cinder_store_user_name'
 #   Defaults to $::os_service_default.
 #
+# [*cinder_mount_point_base*]
+#   (Optional) When glance uses cinder as store and cinder backend is NFS, the mount point
+#    would be required to be set with this parameter.
+#   Defaults to $::os_service_default.
+#
 # [*store_description*]
 #   (optional) Provides constructive information about the store backend to
 #   end users.
@@ -81,6 +86,7 @@ define glance::backend::multistore::cinder(
   $cinder_store_project_name   = $::os_service_default,
   $cinder_store_user_name      = $::os_service_default,
   $cinder_store_password       = $::os_service_default,
+  $cinder_mount_point_base     = $::os_service_default,
   $store_description           = $::os_service_default,
 ) {
 
@@ -96,6 +102,7 @@ define glance::backend::multistore::cinder(
     "${name}/cinder_store_project_name":   value => $cinder_store_project_name;
     "${name}/cinder_store_user_name":      value => $cinder_store_user_name;
     "${name}/cinder_store_password":       value => $cinder_store_password;
+    "${name}/cinder_mount_point_base":     value => $cinder_mount_point_base;
     "${name}/store_description":           value => $store_description;
   }
 
@@ -108,6 +115,7 @@ define glance::backend::multistore::cinder(
     "${name}/cinder_store_auth_address":   value => $cinder_store_auth_address;
     "${name}/cinder_store_project_name":   value => $cinder_store_project_name;
     "${name}/cinder_store_user_name":      value => $cinder_store_user_name;
+    "${name}/cinder_mount_point_base":     value => $cinder_mount_point_base;
     "${name}/cinder_store_password":       value => $cinder_store_password;
   }
 

spec/defines/glance_backend_multistore_cinder_spec.rb
@@ -40,6 +40,7 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_api_config('cinder/cinder_store_project_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('cinder/cinder_store_user_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('cinder/cinder_store_password').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_glance_api_config('cinder/cinder_mount_point_base').with_value('<SERVICE DEFAULT>')
       end
       it 'configures glance-cache.conf' do
         is_expected.to_not contain_glance_cache_config('cinder/store_description')
@@ -52,6 +53,7 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_cache_config('cinder/cinder_store_project_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_user_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_password').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_glance_cache_config('cinder/cinder_mount_point_base').with_value('<SERVICE DEFAULT>')
       end
     end
 
@@ -68,6 +70,7 @@ describe 'glance::backend::multistore::cinder' do
           :cinder_store_project_name   => 'services',
           :cinder_store_user_name      => 'glance',
           :cinder_store_password       => 'glance',
+          :cinder_mount_point_base     => '/var/lib/glance/mnt',
         }
       end
       it 'configures glance-api.conf' do
@@ -81,6 +84,7 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_api_config('cinder/cinder_store_project_name').with_value('services')
         is_expected.to contain_glance_api_config('cinder/cinder_store_user_name').with_value('glance')
         is_expected.to contain_glance_api_config('cinder/cinder_store_password').with_value('glance')
+        is_expected.to contain_glance_api_config('cinder/cinder_mount_point_base').with_value('/var/lib/glance/mnt')
       end
       it 'configures glance-cache.conf' do
         is_expected.to_not contain_glance_cache_config('cinder/store_description')
@@ -93,6 +97,7 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_cache_config('cinder/cinder_store_project_name').with_value('services')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_user_name').with_value('glance')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_password').with_value('glance')
+        is_expected.to contain_glance_cache_config('cinder/cinder_mount_point_base').with_value('/var/lib/glance/mnt')
       end
     end
   end

releasenotes/notes/glance_backend_cinder_mount_point_base-5741d2966cc10c7b.yaml
@@ -1,0 +1,6 @@
+---
+features:
+  - |
+    A new parameter ``cinder_mount_point_base`` is introduced into
+    ``glance::backend::multistore::cinder`` class, so that operators
+    can provide the cinder mount point for NFS volumes.

commit 4b4b990b2e00a59506140ee7398b51bd138cd319

Add parameters for multipath connection in cinder backend
This patch introduced the following 2 parameters into
glance::backend::multistore::cinder class, so that operators can define
multipath usage in volume connection by puppet parameter.

 - cinder_enforce_multipath
 - cinder_use_multipath

Backport note:
Resolved conflicts caused by 2936eb233e0310c0788a3c8ed05c42af388dc6e2
which was backported erlier.

Change-Id: I17af43fe8cdf437c90453d316b66c3ea9d4f2745
(cherry picked from commit 5d2acead72c28910c7712b49bb90ff5f441a79e9)

manifests/backend/multistore/cinder.pp
@@ -66,6 +66,14 @@
 #   (optional) A valid password for the user specified by `cinder_store_user_name'
 #   Defaults to $::os_service_default.
 #
+# [*cinder_enforce_multipath*]
+#   (optional) Enforce multipath usage when attaching a cinder volume
+#   Defaults to $::os_service_default.
+#
+# [*cinder_use_multipath*]
+#   (optional) Flag to identify multipath is supported or not in the deployment
+#   Defaults to $::os_service_default.
+#
 # [*cinder_mount_point_base*]
 #   (Optional) When glance uses cinder as store and cinder backend is NFS, the mount point
 #    would be required to be set with this parameter.
@@ -86,6 +94,8 @@ define glance::backend::multistore::cinder(
   $cinder_store_project_name   = $::os_service_default,
   $cinder_store_user_name      = $::os_service_default,
   $cinder_store_password       = $::os_service_default,
+  $cinder_enforce_multipath    = $::os_service_default,
+  $cinder_use_multipath        = $::os_service_default,
   $cinder_mount_point_base     = $::os_service_default,
   $store_description           = $::os_service_default,
 ) {
@@ -102,6 +112,8 @@ define glance::backend::multistore::cinder(
     "${name}/cinder_store_project_name":   value => $cinder_store_project_name;
     "${name}/cinder_store_user_name":      value => $cinder_store_user_name;
     "${name}/cinder_store_password":       value => $cinder_store_password;
+    "${name}/cinder_enforce_multipath":    value => $cinder_enforce_multipath;
+    "${name}/cinder_use_multipath":        value => $cinder_use_multipath;
     "${name}/cinder_mount_point_base":     value => $cinder_mount_point_base;
     "${name}/store_description":           value => $store_description;
   }
@@ -115,6 +127,8 @@ define glance::backend::multistore::cinder(
     "${name}/cinder_store_auth_address":   value => $cinder_store_auth_address;
     "${name}/cinder_store_project_name":   value => $cinder_store_project_name;
     "${name}/cinder_store_user_name":      value => $cinder_store_user_name;
+    "${name}/cinder_enforce_multipath":    value => $cinder_enforce_multipath;
+    "${name}/cinder_use_multipath":        value => $cinder_use_multipath;
     "${name}/cinder_mount_point_base":     value => $cinder_mount_point_base;
     "${name}/cinder_store_password":       value => $cinder_store_password;
   }

releasenotes/notes/glance-backend-cinder-multipath-d18f73e3a9c55b05.yaml
@@ -1,0 +1,6 @@
+---
+features:
+  - |
+    The new parameters, cinder_enforce_multipath and cinder_use_multipath, are
+    intoroduced into cinder::backend::multistore::cinder class, so that
+    operators can define multipath usage in volume connection.

spec/defines/glance_backend_multistore_cinder_spec.rb
@@ -40,6 +40,8 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_api_config('cinder/cinder_store_project_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('cinder/cinder_store_user_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('cinder/cinder_store_password').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_glance_api_config('cinder/cinder_enforce_multipath').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_glance_api_config('cinder/cinder_use_multipath').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('cinder/cinder_mount_point_base').with_value('<SERVICE DEFAULT>')
       end
       it 'configures glance-cache.conf' do
@@ -53,6 +55,8 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_cache_config('cinder/cinder_store_project_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_user_name').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_password').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_glance_cache_config('cinder/cinder_enforce_multipath').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_glance_cache_config('cinder/cinder_use_multipath').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_cache_config('cinder/cinder_mount_point_base').with_value('<SERVICE DEFAULT>')
       end
     end
@@ -70,6 +74,8 @@ describe 'glance::backend::multistore::cinder' do
           :cinder_store_project_name   => 'services',
           :cinder_store_user_name      => 'glance',
           :cinder_store_password       => 'glance',
+          :cinder_enforce_multipath    => true,
+          :cinder_use_multipath        => true,
           :cinder_mount_point_base     => '/var/lib/glance/mnt',
         }
       end
@@ -84,6 +90,8 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_api_config('cinder/cinder_store_project_name').with_value('services')
         is_expected.to contain_glance_api_config('cinder/cinder_store_user_name').with_value('glance')
         is_expected.to contain_glance_api_config('cinder/cinder_store_password').with_value('glance')
+        is_expected.to contain_glance_api_config('cinder/cinder_enforce_multipath').with_value(true)
+        is_expected.to contain_glance_api_config('cinder/cinder_use_multipath').with_value(true)
         is_expected.to contain_glance_api_config('cinder/cinder_mount_point_base').with_value('/var/lib/glance/mnt')
       end
       it 'configures glance-cache.conf' do
@@ -97,6 +105,8 @@ describe 'glance::backend::multistore::cinder' do
         is_expected.to contain_glance_cache_config('cinder/cinder_store_project_name').with_value('services')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_user_name').with_value('glance')
         is_expected.to contain_glance_cache_config('cinder/cinder_store_password').with_value('glance')
+        is_expected.to contain_glance_cache_config('cinder/cinder_enforce_multipath').with_value(true)
+        is_expected.to contain_glance_cache_config('cinder/cinder_use_multipath').with_value(true)
         is_expected.to contain_glance_cache_config('cinder/cinder_mount_point_base').with_value('/var/lib/glance/mnt')
       end
     end