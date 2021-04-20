dd731f5d600ac15c1e74068cb267e5421ddd53c5
Takashi Kajinami <tkajinam@redhat.com>
2021-04-20 23:48:46 +0900

    Add support for healthcheck middleware options
    Depends-on: https://review.opendev.org/772202
    Change-Id: If0aa39e31bd59a0a14c24f4835a2c355305798b3

---
+++ spec/classes/glance_healthcheck_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe 'glance::healthcheck' do
+
+  shared_examples_for 'glance::healthcheck' do
+
+    context 'with default parameters' do
+      let :params do
+        {}
+      end
+
+      it 'configures default values' do
+        is_expected.to contain_oslo__healthcheck('glance_api_config').with(
+          :detailed              => '<SERVICE DEFAULT>',
+          :backends              => '<SERVICE DEFAULT>',
+          :disable_by_file_path  => '<SERVICE DEFAULT>',
+          :disable_by_file_paths => '<SERVICE DEFAULT>',
+        )
+      end
+    end
+
+    context 'with specific parameters' do
+      let :params do
+        {
+          :detailed              => true,
+          :backends              => ['disable_by_file'],
+          :disable_by_file_path  => '/etc/glance/healthcheck/disabled',
+          :disable_by_file_paths => ['9292:/etc/glance/healthcheck/disabled'],
+        }
+      end
+
+      it 'configures specified values' do
+        is_expected.to contain_oslo__healthcheck('glance_api_config').with(
+          :detailed              => true,
+          :backends              => ['disable_by_file'],
+          :disable_by_file_path  => '/etc/glance/healthcheck/disabled',
+          :disable_by_file_paths => ['9292:/etc/glance/healthcheck/disabled'],
+        )
+      end
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_configures 'glance::healthcheck'
+    end
+  end
+
+end
