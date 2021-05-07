0dcab2950dd57c02fcb0d9864341dcb2f2b736e3
Takashi Kajinami <tkajinam@redhat.com>
2021-05-07 20:41:46 +0900

    Swift backend: Add the swift_store_service_type parameter
    Change-Id: I22a63e40b5981ec79f93922bed0b9f62f83b916d

--- spec/defines/glance_backend_multistore_swift_spec.rb
+++ spec/defines/glance_backend_multistore_swift_spec.rb
@@ -45,6 +45,7 @@ describe 'glance::backend::multistore::swift' do
         is_expected.to contain_glance_api_config('swift/swift_store_container').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('swift/swift_store_create_container_on_put').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('swift/swift_store_endpoint_type').with_value('internalURL')
+        is_expected.to contain_glance_api_config('swift/swift_store_service_type').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('swift/swift_store_region').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_glance_api_config('swift/swift_store_config_file').with_value('/etc/glance/glance-swift.conf')
         is_expected.to contain_glance_api_config('swift/default_swift_reference').with_value('ref1')
@@ -74,6 +75,7 @@ describe 'glance::backend::multistore::swift' do
           :swift_store_container               => 'swift',
           :swift_store_create_container_on_put => true,
           :swift_store_endpoint_type           => 'publicURL',
+          :swift_store_service_type            => 'object-store',
           :swift_store_region                  => 'RegionTwo',
           :swift_store_config_file             => '/etc/glance/glance-swift2.conf',
           :default_swift_reference             => 'swift_creds',
@@ -89,6 +91,7 @@ describe 'glance::backend::multistore::swift' do
         is_expected.to contain_glance_api_config('swift/swift_store_large_object_size').with_value('100')
         is_expected.to contain_glance_api_config('swift/swift_store_large_object_chunk_size').with_value('50')
         is_expected.to contain_glance_api_config('swift/swift_store_endpoint_type').with_value('publicURL')
+        is_expected.to contain_glance_api_config('swift/swift_store_service_type').with_value('object-store')
         is_expected.to contain_glance_api_config('swift/swift_store_region').with_value('RegionTwo')
         is_expected.to contain_glance_api_config('swift/swift_store_config_file').with_value('/etc/glance/glance-swift2.conf')
         is_expected.to contain_glance_api_config('swift/default_swift_reference').with_value('swift_creds')
