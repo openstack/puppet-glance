28de7383cf9fb9de4b43597ada762a79fceb46b6
Thomas Goirand <zigo@debian.org>
2021-05-21 11:15:11 +0200

    Get rid of the $pyvers variable
    Since everyone has switched to Python3, it's time for the removal of the
    $pyvers variable.
    
    Change-Id: I3878af33cb0deeddeb3d7b299525201edcc65976

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,9 +2,8 @@
 # should be considered to be constant
 class glance::params {
   include openstacklib::defaults
-  $pyvers = $::openstacklib::defaults::pyvers
 
-  $client_package_name = "python${pyvers}-glanceclient"
+  $client_package_name = 'python3-glanceclient'
 
   $cache_cleaner_command = 'glance-cache-cleaner'
   $cache_pruner_command  = 'glance-cache-pruner'
@@ -15,16 +14,16 @@ class glance::params {
       $package_name          = 'openstack-glance'
       $api_package_name      = undef
       $api_service_name      = 'openstack-glance-api'
-      $pyceph_package_name   = "python${pyvers}-rbd"
+      $pyceph_package_name   = 'python3-rbd'
     }
     'Debian': {
       $package_name          = undef
       $api_package_name      = 'glance-api'
       $api_service_name      = 'glance-api'
       if $::os_package_type == 'debian' {
-        $pyceph_package_name = "python${pyvers}-ceph"
+        $pyceph_package_name = 'python-ceph'
       } else {
-        $pyceph_package_name = "python${pyvers}-rbd"
+        $pyceph_package_name = 'python3-rbd'
       }
     }
     default: {
