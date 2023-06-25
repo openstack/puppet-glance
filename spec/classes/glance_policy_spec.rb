dcacc06e303c75c368040bfbaf91752d97b1a5b6
Tobias Urdin <tobias.urdin@binero.com>
2023-06-25 23:43:55 +0200

    Add per module policy service refresh
    Updating the policies for this project should only
    refresh the services that reads it.
    
    Change-Id: I83d3ff31083dd687c5b2d76c8297b5e74a7caa1a
    (cherry picked from commit 8d4873a3af5e515769c7b87ce5d36cc49b773707)

--- spec/classes/glance_policy_spec.rb
+++ spec/classes/glance_policy_spec.rb
@@ -33,6 +33,7 @@ describe 'glance::policy' do
           :file_group   => 'glance',
           :file_format  => 'yaml',
           :purge_config => false,
+          :tag          => 'glance',
         )
         is_expected.to contain_oslo__policy('glance_api_config').with(
           :enforce_scope        => false,
@@ -63,6 +64,7 @@ describe 'glance::policy' do
           :file_group   => 'glance',
           :file_format  => 'yaml',
           :purge_config => true,
+          :tag          => 'glance',
         )
         is_expected.to contain_oslo__policy('glance_api_config').with(
           :enforce_scope        => false,
