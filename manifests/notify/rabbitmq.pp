953143d0b75eb6b0e996c511bd907c07cd146ace
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-03-14 09:25:34 +0900

    Expose rabbit_transient_quorum_queue
    Depends-on: https://review.opendev.org/911021
    Change-Id: I1ff921226e2b5b2da6f556b7e7ead7bdbfcebaa0

--- manifests/notify/rabbitmq.pp
+++ manifests/notify/rabbitmq.pp
@@ -58,6 +58,10 @@
 #   (Optional) Use quorum queues in RabbitMQ.
 #   Defaults to $facts['os_service_default']
 #
+# [*rabbit_transient_quorum_queue*]
+#   (Optional) Use quorum queues for transients queues in RabbitMQ.
+#   Defaults to $facts['os_service_default']
+#
 # [*rabbit_quorum_delivery_limit*]
 #   (Optional) Each time a message is rdelivered to a consumer, a counter is
 #   incremented. Once the redelivery count exceeds the delivery limit
@@ -136,6 +140,7 @@ class glance::notify::rabbitmq(
   $rabbit_heartbeat_rate              = $facts['os_service_default'],
   $rabbit_heartbeat_in_pthread        = $facts['os_service_default'],
   $rabbit_quorum_queue                = $facts['os_service_default'],
+  $rabbit_transient_quorum_queue      = $facts['os_service_default'],
   $rabbit_quorum_delivery_limit       = $facts['os_service_default'],
   $rabbit_quorum_max_memory_length    = $facts['os_service_default'],
   $rabbit_quorum_max_memory_bytes     = $facts['os_service_default'],
@@ -169,6 +174,7 @@ class glance::notify::rabbitmq(
     amqp_durable_queues             => $amqp_durable_queues,
     kombu_compression               => $kombu_compression,
     rabbit_quorum_queue             => $rabbit_quorum_queue,
+    rabbit_transient_quorum_queue   => $rabbit_transient_quorum_queue,
     rabbit_quorum_delivery_limit    => $rabbit_quorum_delivery_limit,
     rabbit_quorum_max_memory_length => $rabbit_quorum_max_memory_length,
     rabbit_quorum_max_memory_bytes  => $rabbit_quorum_max_memory_bytes,
