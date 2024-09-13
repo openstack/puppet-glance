48a090268ff1b974214edc43676bca33ee0e17e8
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-13 11:29:13 +0900

    Expose rabbit_qos_prefetch_count
    The option was supported by puppet-oslo but was not configurable via
    this module.
    
    Change-Id: Idd8cda64a9fb507b15265e155ed55a5bcee8c17b

--- manifests/notify/rabbitmq.pp
+++ manifests/notify/rabbitmq.pp
@@ -54,6 +54,10 @@
 #   will be run through a green thread.
 #   Defaults to $facts['os_service_default']
 #
+# [*rabbit_qos_prefetch_count*]
+#   (Optional) Specifies the number of messages to prefetch.
+#   Defaults to $facts['os_service_default']
+#
 # [*rabbit_quorum_queue*]
 #   (Optional) Use quorum queues in RabbitMQ.
 #   Defaults to $facts['os_service_default']
@@ -139,6 +143,7 @@ class glance::notify::rabbitmq(
   $rabbit_heartbeat_timeout_threshold = $facts['os_service_default'],
   $rabbit_heartbeat_rate              = $facts['os_service_default'],
   $rabbit_heartbeat_in_pthread        = $facts['os_service_default'],
+  $rabbit_qos_prefetch_count          = $facts['os_service_default'],
   $rabbit_quorum_queue                = $facts['os_service_default'],
   $rabbit_transient_quorum_queue      = $facts['os_service_default'],
   $rabbit_quorum_delivery_limit       = $facts['os_service_default'],
@@ -164,6 +169,7 @@ class glance::notify::rabbitmq(
     heartbeat_timeout_threshold     => $rabbit_heartbeat_timeout_threshold,
     heartbeat_rate                  => $rabbit_heartbeat_rate,
     heartbeat_in_pthread            => $rabbit_heartbeat_in_pthread,
+    rabbit_qos_prefetch_count       => $rabbit_qos_prefetch_count,
     rabbit_use_ssl                  => $rabbit_use_ssl,
     kombu_ssl_ca_certs              => $kombu_ssl_ca_certs,
     kombu_ssl_certfile              => $kombu_ssl_certfile,
